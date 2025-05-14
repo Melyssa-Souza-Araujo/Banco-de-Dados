3c8f8d72198d3a50385676182d64d1fab4b6737c
MELYSSA SOUZA ARAUJO <126412768+Melyssa-Souza-Araujo@users.noreply.github.com>
2025-05-14 15:08:33 -0300

    Update and rename script select.sql to script-select.sql

--- script-select.sql
+++ script-select.sql
@@ -2,24 +2,24 @@ USE universidade;
 
 SELECT * FROM turmas;
 
--- exercício 1
+-- exercÃ­cio 1
 SELECT * FROM docentes;
 
--- exercício 2
+-- exercÃ­cio 2
 SELECT cd_docente, nm_docente 
 FROM docentes;
 
--- exercício 3
+-- exercÃ­cio 3
 SELECT * FROM disciplinas;
 
--- exercício 4
+-- exercÃ­cio 4
 SELECT * FROM alunos;
 
--- exercício 5
+-- exercÃ­cio 5
 SELECT nm_aluno, dt_matricula 
 FROM alunos;
 
--- exercício 6
+-- exercÃ­cio 6
 SELECT * FROM notas;
 
 
@@ -29,36 +29,36 @@ WHERE cd_turma <= 4
 SELECT * FROM turmas
 WHERE ds_turma LIKE '%A%'
 
--- exercício 1 where
+-- exercÃ­cio 1 where
 SELECT * FROM docentes
 WHERE cd_docente = '9233251013'
 
--- exercício 2 where
+-- exercÃ­cio 2 where
 SELECT * FROM alunos
 WHERE cd_turma = 6
 
--- exercício 3 where
+-- exercÃ­cio 3 where
 SELECT * FROM alunos
 WHERE dt_matricula > 2024-03-31
 
---  exercício 4 where
+--  exercÃ­cio 4 where
 SELECT * FROM alunos
 WHERE cd_matricula LIKE 'G%'
 
---  exercício 5 where
+--  exercÃ­cio 5 where
 SELECT * FROM disciplinas
 WHERE ds_disciplina LIKE '%SISTEMA%'
 
---  exercício 6 where
+--  exercÃ­cio 6 where
 SELECT * FROM disciplinas
 WHERE cd_docente LIKE '%9233199257%'
 
--- exercício 8 where (o professor decidiu pular o 7)
+-- exercÃ­cio 8 where (o professor decidiu pular o 7)
 SELECT * FROM disciplinas
 WHERE ds_disciplina LIKE '%SISTEMA%' 
 OR ds_disciplina LIKE '%COMPUTADOR%'
 
---  exercício 9 where
+--  exercÃ­cio 9 where
 SELECT * FROM disciplinas
 WHERE (ds_disciplina LIKE '%SISTEMA%' 
 OR ds_disciplina LIKE '%COMPUTADOR%')
